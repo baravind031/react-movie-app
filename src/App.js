c276c9a94dc9d16e547be8be5aa06058ff06fe3f
Aravind <baravind031@gmail.com>
2023-05-25 11:55:29 +0530

    modified  files.

--- src/App.js
+++ src/App.js
@@ -1,123 +1,77 @@
 import React, { useState } from "react";
 import axios from "axios";
-import Search from "./components/Search";
-import Results from "./components/Results";
-import Detail from "./components/Details";
+import SearchResults from "./components/SearchResults";
+import MovieDetails from "./components/MovieDetails";
+import Recommendations from "./components/Recommendations";
 import "./App.css";
+
+
 function App() {
-const [state, setState] = useState({
-	s: "sherlock",
-	results: [],
-	selected: {},
-});
-const apiurl = "https://www.omdbapi.com/?apikey=a2526df0";
-// const apiurl='http://www.omdbapi.com/?i=tt3896198&apikey=3be0bc37';
-const searchInput = (e) => {
-	let s = e.target.value;
+     const [searchTerm, setSearchTerm] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
+     const [selectedMovie, setSelectedMovie] = useState(null);
+     const [recommendations, setRecommendations] = useState([]);
 
-	setState((prevState) => {
-	return { ...prevState, s: s };
-	});
-};
+   const searchMovies = async () => {
+     
+      const response = await axios.get(
+        `http://www.omdbapi.com/?apikey=3be0bc37&s=${searchTerm}`
+      );
+      setSearchResults(response.data.Search || []);
+    
+  };
 
-const search = (e) => {
-	if (e.key === "Enter") {
-	axios(apiurl + "&s=" + state.s).then(({ data }) => {
-		let results = data.Search;
+  const selectMovie = async (imdbID) => {
+     
+      const response = await axios.get(
+        `http://www.omdbapi.com/?apikey=3be0bc37&i=${imdbID}`
+      );
+      setSelectedMovie(response.data);
+      getRecommendations(response.data.Genre);
+  };
 
-		console.log(results);
+  const getRecommendations = async (genre) => {
+    
+      const response = await axios.get(
+        `http://www.omdbapi.com/?apikey=3be0bc37&s=&type=movie&genre=${genre}`
+      );
+      setRecommendations(response.data.Search || []);
+    
+  };
 
-		setState((prevState) => {
-		return { ...prevState, results: results };
-		});
-	});
-	}
-};
+  const resetSelectedMovie = () => {
+    setSelectedMovie(null);
+    setRecommendations([]);
+  };
 
-const openDetail = (id) => {
-	axios(apiurl + "&i=" + id).then(({ data }) => {
-	let result = data;
-
-	setState((prevState) => {
-		return { ...prevState, selected: result };
-	});
-	});
-};
-
-const closeDetail = () => {
-	setState((prevState) => {
-	return { ...prevState, selected: {} };
-	});
-};
-
-return (
-	<div className="App">
-	<header className="App-header">
-		<h1>PVR</h1>
-	</header>
-	<main>
-		<Search searchInput={searchInput} search={search} />
-
-		<Results results={state.results} openDetail={openDetail} />
-
-		{typeof state.selected.Title != "undefined" ? (
-		<Detail selected={state.selected} closeDetail={closeDetail} />
-		) : (
-		false
-		)}
-	</main>
-	</div>
-);
+  return (
+    <div className="container">
+      <h1>Find your Movie </h1>
+      {! selectedMovie ? (
+        <>
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button onClick={searchMovies}>Search</button>
+          <SearchResults
+            results={searchResults}
+            onSelectMovie={selectMovie}
+          />
+        </>
+      ) : (
+        <>
+          <MovieDetails movie={selectedMovie} />
+          <Recommendations
+            recommendations={recommendations}
+            onReset={resetSelectedMovie}
+          />
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
 
-
-
-
-// import React, { useState, useEffect } from 'react';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './App.css';
-// import MovieListHeading from './Components/MovieListHeading';
-// import SearchBox from './Components/SearchBox';
-
-// const App = () => {
-// 	const [movies, setMovies] = useState([]);
-//  const [searchValue, setSearchValue] = useState('');
-// const getMovieRequest = async (searchValue) => {
-
-//     const url =` http://www.omdbapi.com/?s=${searchValue}&apikey=3be0bc37`;
-
-//     // The fetch function is used to send a GET request to the specified url and 
-//     // The await keyword is used to pause the execution of the function until the response is received
-//     // The response object contains the HTTP response received from the API. 
-// 		const response = await fetch(url);
-// 		const responseJson = await response.json();
-
-// // The response object contains the HTTP response received from the API
-// // The responseJson object contains the JSON data returned from the API
-// 		if (responseJson.Search) {
-// 			setMovies(responseJson.Search);
-// 		}
-
-//     //  setMovies update function need to write a code to update the movies
-// 	};
-
-
-// 	return (
-// 		<div className='container-fluid movie-app'>  
-
-     
-// 		  	<div className='row d-flex align-items-center mt-4 mb-4'>
-
-//           {/*  The MovieListHeading component is responsible for rendering a title for the movie */}
-// 				<MovieListHeading heading='Movies'/>
-
-//         {/* The SearchBox componentform that 
-//         allows users to input a search query and search action will be done */}
-// 				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
-// 			 </div>			 
-// 		</div>
-// 	);
-// };
-// export default App;
